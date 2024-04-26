f5c3211c787a45e6dd764bc7465dedc69358cd78
Denys1994q <drybachok@gmail.com>
2024-04-26 19:21:45 +0300

    fix spinner component

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,29 @@ import './App.sass';
 import {Outlet} from 'react-router-dom';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
-import {FC, useEffect} from 'react';
+import {useEffect} from 'react';
 import {useAppDispatch} from '@/store/hooks';
 import {getUser} from '@/store/slices/auth/auth.thunks';
-import Spinner from '@/components/Spinner/Spinner';
-import BasicBackDrop from '@/components/BasicBackDrop/BasicBackDrop';
 import useAxiosInterceptor from './hooks/useAxiosInterceptor';
-import {useAppSelector} from '@/store/hooks';
-import {Box} from '@mui/material';
+import Spinner from './components/Spinner/Spinner';
 
-const App: FC = () => {
+const App = () => {
   useAxiosInterceptor();
   const dispatch = useAppDispatch();
-  const isLoading = useAppSelector((store) => store.loadingSlice.isLoading);
 
   useEffect(() => {
     dispatch(getUser());
   }, []);
 
   return (
-    <>
-      <div className="main-container">
-        <Header />
-        <div className="content-container">
-          <Outlet />
-        </div>
-        <Footer />
+    <div className="main-container">
+      <Header />
+      <div className="content-container">
+        <Outlet />
       </div>
-      <BasicBackDrop isOpen={isLoading}>
-        <Box sx={{width: '100%', position: 'absolute', top: '2px'}}>
-          <Spinner />
-        </Box>
-      </BasicBackDrop>
-    </>
+      <Footer />
+      <Spinner />
+    </div>
   );
 };
 
--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,8 +1,18 @@
-import {FC} from 'react';
 import LinearProgress from '@mui/material/LinearProgress';
+import BasicBackDrop from '../BasicBackDrop/BasicBackDrop';
+import {useAppSelector} from '@/store/hooks';
+import {Box} from '@mui/material';
 
-const Spinner: FC = () => {
-  return <LinearProgress color="success" />;
+const Spinner = () => {
+  const isLoading = useAppSelector((store) => store.loadingSlice.isLoading);
+
+  return (
+    <BasicBackDrop isOpen={isLoading}>
+      <Box sx={{width: '100%', position: 'absolute', top: '2px'}}>
+        <LinearProgress color="success" />
+      </Box>
+    </BasicBackDrop>
+  );
 };
 
 export default Spinner;
