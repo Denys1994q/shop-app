f9e9e909466808bf8e2c9d46c161822988e9329c
Denys1994q <drybachok@gmail.com>
2024-05-07 12:37:37 +0300

    fix toast slice initial value

--- src/store/slices/toast/toast.slice.ts
+++ src/store/slices/toast/toast.slice.ts
@@ -1,10 +1,9 @@
 import {PayloadAction, createSlice} from '@reduxjs/toolkit';
 import {Toast, ToastState} from './toast.model';
-import {ToastEnum} from '@/models/toast.enum';
 
 const initialState: ToastState = {
   isOpen: false,
-  toast: {message: 'keke', type: ToastEnum.INFO}
+  toast: null
 };
 
 const ToastSlice = createSlice({
